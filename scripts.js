07ebca1360d8daba72a92a334976a1e669e79e72
natalie815 <luyuanyuan815@gmail.com>
2023-03-29 12:35:37 -0700

    web

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
 function toggleMenu() {
   var menu = document.getElementById("menu");
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
+  var icon = document.getElementsByClassName("menu-icon")[0];
+  if (menu.style.display === "none") {
     menu.style.display = "block";
+    icon.style.color = "#fff";
+    icon.style.background = "#333";
+  } else {
+    menu.style.display = "none";
+    icon.style.color = "#333";
+    icon.style.background = "none";
   }
+}
+
+function toggleSearch() {
+  var searchForm = document.querySelector('.search-form');
+  searchForm.style.display = searchForm.style.display === 'block' ? 'none' : 'block';
+}
+
+function searchFunction() {
+  var input = document.querySelector('.search-form input');
+  alert('You searched for: ' + input.value);
+  input.value = '';
+  return false;
 }
